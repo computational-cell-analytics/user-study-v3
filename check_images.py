898bf65510f6869db47ed8d0bbceb81f60d4f82c
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2024-07-09 12:03:27 +0200

    Update image checking

--- check_images.py
+++ check_images.py
@@ -1,22 +1,53 @@
+import os
 from glob import glob
+from pathlib import Path
 
 import imageio.v3 as imageio
 import napari
 
+from skimage.measure import label
+
 
 def check_reference_images():
-    images = sorted(glob("data/reference_data/images/*.tif"))
-    masks = sorted(glob("data/reference_data/masks/*.tif"))
+    images = sorted(glob("data/ground_truth/images/*.tif"))
+    masks_v1 = "data/ground_truth/masks_v1"
+    masks_v2 = "data/ground_truth/masks_v2"
+    results = "data/ground_truth/masks"
+    os.makedirs(results, exist_ok=True)
 
     print("Number of annotated images:", len(images))
 
-    for im_path, mask_path in zip(images, masks):
+    for im_path in images:
+        name = Path(im_path).stem
+
+        v1_path = os.path.join(masks_v1, f"{name}.tif")
+        mask = imageio.imread(v1_path)
+        load_path = v1_path
+
+        v2_path = os.path.join(masks_v2, f"{name}_cp_masks.png")
+        if os.path.exists(v2_path):
+            mask = imageio.imread(v2_path)
+            load_path = v2_path
+
+        result_path = os.path.join(results, f"{name}.tif")
+        if os.path.exists(result_path):
+            mask = imageio.imread(result_path)
+            load_path = result_path
+
         image = imageio.imread(im_path)
-        mask = imageio.imread(mask_path)
 
         v = napari.Viewer()
         v.add_image(image)
         v.add_labels(mask)
+        v.title = load_path
+
+        @v.bind_key("s")
+        def _save(viewer):
+            mask = viewer.layers["mask"].data
+            mask = label(mask)
+            print("Saving mask to", result_path)
+            imageio.imwrite(result_path, mask, compression="zlib")
+
         napari.run()
 
 
