c72c43f3f9eb2dad45b0852b8aee480b86c914bc
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2024-07-27 18:02:52 +0200

    Add distance unet training and evaluation

--- train_and_eval/distance_unet/train_distance_unet.py
+++ train_and_eval/distance_unet/train_distance_unet.py
@@ -4,10 +4,11 @@ import os
 import torch_em
 
 from torch_em.model import UNet2d
-from torch_em.data.datasets import get_organoidnet_loader
+from torch_em.data.datasets import get_organoidnet_loader, get_organoidnet_dataset
 
 
-ROOT = "../data"
+# ROOT = "../data"
+ROOT = "/scratch-emmy/projects/nim00007/user-study/data"
 
 
 def _get_orga_loader(batch_size, label_transform):
@@ -59,15 +60,55 @@ def _get_organoidnet_loader(batch_size, label_transform):
     return train_loader, val_loader
 
 
+def _get_combined_loader(batch_size, label_transform):
+    path_organoidnet = os.path.join(ROOT, "datasets", "organoidnet")
+    path_orga = os.path.join(ROOT, "datasets", "orga", "v1")
+    patch_shape = (512, 512)
+
+    train_images = os.path.join(path_orga, "train", "images")
+    train_labels = os.path.join(path_orga, "train", "masks")
+    train_ds = [
+        get_organoidnet_dataset(
+            path_organoidnet, split="Training", patch_shape=patch_shape, label_transform=label_transform
+        ),
+        torch_em.default_segmentation_dataset(
+            raw_paths=train_images, label_paths=train_labels, raw_key="*.tif", label_key="*.tif",
+            patch_shape=(1,) + patch_shape, ndim=2,
+            label_transform=label_transform, is_seg_dataset=True, n_samples=500,
+        )
+    ]
+    train_ds = torch_em.data.ConcatDataset(*train_ds)
+    train_loader = torch_em.get_data_loader(train_ds, batch_size=batch_size, num_workers=6, shuffle=True)
+
+    val_images = os.path.join(path_orga, "val", "images")
+    val_labels = os.path.join(path_orga, "val", "masks")
+    val_ds = [
+        get_organoidnet_dataset(
+            path_organoidnet, split="Validation", patch_shape=patch_shape, label_transform=label_transform
+        ),
+        torch_em.default_segmentation_dataset(
+            raw_paths=val_images, label_paths=val_labels, raw_key="*.tif", label_key="*.tif",
+            patch_shape=(1,) + patch_shape, ndim=2,
+            label_transform=label_transform, is_seg_dataset=True, n_samples=40,
+        )
+    ]
+    val_ds = torch_em.data.ConcatDataset(*val_ds)
+    val_loader = torch_em.get_data_loader(val_ds, batch_size=batch_size, num_workers=6, shuffle=True)
+
+    return train_loader, val_loader
+
+
 def get_loaders(ds_name, batch_size, label_transform):
     if ds_name == "orga/v1":
         return _get_orga_loader(batch_size, label_transform)
+    elif ds_name == "combined":
+        return _get_combined_loader(batch_size, label_transform)
     else:
         return _get_organoidnet_loader(batch_size, label_transform)
 
 
 def train_unet(ds_name):
-    assert ds_name in ("orga/v1", "organoidnet")
+    assert ds_name in ("orga/v1", "organoidnet", "combined")
     model = UNet2d(in_channels=1, out_channels=3, initial_features=64, final_activation="Sigmoid")
 
     batch_size = 4
